ce01774b687615d5344060605e725be7ebe43bfd
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2024-02-29 23:27:37 +0530

    C Program to delet data from the  End of an array

--- Dsa Practice Codes/Arrays/delet_from_begin.c
+++ Dsa Practice Codes/Arrays/delet_from_begin.c
@@ -1,4 +1,4 @@
-//C Program to delet data from Begining in an array
+ //C Program to delet data from Begining in an array
 
 #include <stdio.h>
 #define MAX_SIZE 50
---
+++ Dsa Practice Codes/Arrays/delet_from_end.c
@@ -0,0 +1,44 @@
+ //C Program to delet data from the  End of an array
+
+#include <stdio.h>
+#define MAX_SIZE 50
+
+int main () {
+    int arr[MAX_SIZE];
+    int i , size , del_item ;
+
+    //Input Array size
+    printf("Enter The Size of Array : ");
+    scanf("%d",&size);
+
+    //Exiting program due to invalid array size 
+
+    if (size <= 0 || size >MAX_SIZE ) {
+        printf("Invalid Array Size ");
+        return 1;
+    }
+
+    else {
+
+        //For insertion of Array
+        printf("Enter the elements of the Array : ");
+        for(i=0 ; i<size ; i++){
+                scanf("%d",&arr[i]);
+        }
+
+        //Print the item to be deleted 
+        del_item = arr[size-1];
+        printf("Deleted Data item is :  %d\n",del_item);
+
+        //for deleting data from the end of array
+        size--;
+
+        //Printing the array after deletion
+        printf("Upodated array elements are : ");
+
+        for (i = 0 ; i <size ; i++){
+            printf(" %d  ",arr[i]);
+        }
+    }
+    return 0;
+}
